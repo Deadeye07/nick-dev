6b294adeb1527757426c6790913bf0052213cb89
Nick <nhranitz@gmail.com>
2022-12-07 18:20:11 -0500

    Skills Heading Fontsize updates

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -4,12 +4,12 @@ import SkillsText from "./skills.json";
 export default function Skills() {
   const data = SkillsText;
   return (
-    <Tab.Panel className="rounded-xl bg-white dark:bg-slate-700 p-3">
+    <Tab.Panel className="rounded-xl bg-white dark:bg-slate-700 ">
       <ul>
-        <li className="relative rounded-md p-3">
+        <li className="relative rounded-md pt-3 px-3">
           {data.map(({ id, title, items }) => (
-            <div className="my-6" key={id}>
-              <div className="text-xl mb-2 font-bold">{title}</div>
+            <div className="mb-6" key={id}>
+              <div className="text-lg font-bold leading-5 mb-2">{title}</div>
               <div className="border-2 rounded-xl border-purple-500/20 font-bold flex flex-row text-center flex-wrap justify-center">
                 {items.map((text) => (
                   <div key={text} className="mx-2 my-2">
